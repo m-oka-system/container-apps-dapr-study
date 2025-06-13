a67fb5e26fb059e1168e0d4d3fdb29d3c57317ae
m-oka-system <m-oka-system@outlook.com>
2025-06-13 15:34:52 +0900

    [update]ServerActionのallowedOriginsにカスタムドメインを追加 (X-Forwarded-Hostエラー対応)

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        process.env.CUSTOM_DOMAIN,
+        `*.${process.env.CUSTOM_DOMAIN}`,
+      ],
+    },
+  },
 };
 
 export default nextConfig;
